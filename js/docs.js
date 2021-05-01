5b4280a99447cb358e6b34706d96bfc0f08ddad2
Spencer Peace <peaces2@nku.edu>
2021-05-01 15:01:15 -0400

    card fade-ins and card spacing on desktop

--- js/docs.js
+++ js/docs.js
@@ -1,3 +1,10 @@
+$(document).ready(function() { 
+    $('.card').each(function(fadeInDiv){
+        $(this).delay(fadeInDiv * 250).fadeIn(1000);
+    });
+    $('.page-footer').fadeIn(1000);
+ });
+
 function myFunction(x) {
     x.classList.toggle("change");
 }
